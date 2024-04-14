338c2cd405b9ed2a6cc09cb39c44aacf1ff336cc
Tarek Tarabichi <ttarabichi@gmail.com>
2024-04-14 07:43:06 +0400

    Add files via upload

---
+++ database_config.php
@@ -0,0 +1,14 @@
+<?php
+// database_config.php
+
+// Database host
+	define('DB_HOST', 'localhost');
+
+// Database username
+	define('DB_USER', 'root');
+
+// Database password
+	define('DB_PASS', '');
+
+// Database name
+	define('DB_NAME', 'phpbot');
